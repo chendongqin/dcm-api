ab14006a9a2b09491fc7f8934f4453d1c96084c3
dong <dong@163.com>
2021-07-19 22:05:04 +0800

    feature:达人直播分析直播间列表

--- models/es/es_live_model.go
+++ models/es/es_live_model.go
@@ -1,13 +1,14 @@
 package es
 
 type EsAuthorLiveRoom struct {
-	RoomID       string  `json:"room_id"`
-	AuthorID     string  `json:"author_id"`
-	Title        string  `json:"title"`
-	CreateTime   string  `json:"create_time"`
-	MaxUserCount int     `json:"max_user_count"`
-	NumProducts  int     `json:"num_products"`
-	Sales        float64 `json:"sales"`
-	Gmv          float64 `json:"gmv"`
-	Cover        string  `json:"cover"`
+	RoomID          string  `json:"room_id"`
+	AuthorID        string  `json:"author_id"`
+	Title           string  `json:"title"`
+	CreateTime      string  `json:"create_time"`
+	CreateTimestamp int64   `json:"create_timestamp"`
+	MaxUserCount    int     `json:"max_user_count"`
+	NumProducts     int     `json:"num_products"`
+	Sales           float64 `json:"sales"`
+	Gmv             float64 `json:"gmv"`
+	Cover           string  `json:"cover"`
 }
