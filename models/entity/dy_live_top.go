17fbce429856a8203d152425c50259692bb1c472
dong <dong@163.com>
2021-08-07 15:14:58 +0800

    feature：直播小时榜单

--- models/entity/dy_live_top.go
+++ models/entity/dy_live_top.go
@@ -13,21 +13,23 @@ type DyLiveTopRanks struct {
 }
 
 type LiveRankLiveInfo struct {
-	Cover      string `json:"cover"`
-	CreateTime int    `json:"create_time"`
-	Tag        string `json:"tag"`
-	Title      string `json:"title"`
-	TotalUser  int    `json:"total_user"`
-	UserCount  int    `json:"user_count"`
-	User       struct {
-		Avatar        string `json:"avatar"`
-		DisplayId     string `json:"display_id"`
-		FollowerCount int    `json:"follower_count"`
-		Gender        int    `json:"gender"`
-		Id            string `json:"id"`
-		Nickname      string `json:"nickname"`
-		ShortId       string `json:"short_id"`
-	} `json:"user"`
+	Cover      string         `json:"cover"`
+	CreateTime int            `json:"create_time"`
+	Tag        string         `json:"tag"`
+	Title      string         `json:"title"`
+	TotalUser  int            `json:"total_user"`
+	UserCount  int            `json:"user_count"`
+	User       LiveRankAuthor `json:"user"`
+}
+
+type LiveRankAuthor struct {
+	Avatar        string `json:"avatar"`
+	DisplayId     string `json:"display_id"`
+	FollowerCount int    `json:"follower_count"`
+	Gender        int    `json:"gender"`
+	Id            string `json:"id"`
+	Nickname      string `json:"nickname"`
+	ShortId       string `json:"short_id"`
 }
 
 type DyLiveRank struct {
