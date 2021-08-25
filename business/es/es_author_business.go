d9833c7b26d7df8bdaed04fbd97b10d04bf01ea3
dong <dong@163.com>
2021-08-25 16:09:04 +0800

    fix:达人视频概览

--- business/es/es_author_business.go
+++ business/es/es_author_business.go
@@ -74,9 +74,34 @@ func (receiver *EsAuthorBusiness) BaseSearch(
 	}
 	if category != "" {
 		if category == "其他" {
-			category = ""
+			esQuery.AddCondition(map[string]interface{}{
+				"bool": map[string]interface{}{
+					"should": []map[string]interface{}{
+						{
+							"match_phrase": map[string]interface{}{
+								"tags": map[string]interface{}{
+									"query": "",
+								},
+							},
+						},
+						{
+							"match_phrase": map[string]interface{}{
+								"tags": map[string]interface{}{
+									"query": "0",
+								},
+							},
+						},
+						{
+							"term": map[string]interface{}{
+								"tags": category,
+							},
+						},
+					},
+				},
+			})
+		} else {
+			esQuery.SetTerm("tags.keyword", category)
 		}
-		esQuery.SetTerm("tags.keyword", category)
 	}
 	if sellTags != "" {
 		esQuery.SetTerm("rank_sell_tags.keyword", sellTags)
@@ -194,9 +219,34 @@ func (receiver *EsAuthorBusiness) SimpleSearch(
 	esQuery.SetTerm("exist", 1)
 	if tags != "" {
 		if tags == "其他" {
-			tags = ""
+			esQuery.AddCondition(map[string]interface{}{
+				"bool": map[string]interface{}{
+					"should": []map[string]interface{}{
+						{
+							"match_phrase": map[string]interface{}{
+								"tags": map[string]interface{}{
+									"query": "",
+								},
+							},
+						},
+						{
+							"match_phrase": map[string]interface{}{
+								"tags": map[string]interface{}{
+									"query": "0",
+								},
+							},
+						},
+						{
+							"term": map[string]interface{}{
+								"tags": tags,
+							},
+						},
+					},
+				},
+			})
+		} else {
+			esQuery.SetTerm("tags.keyword", tags)
 		}
-		esQuery.SetTerm("tags.keyword", tags)
 	}
 	if secondTags != "" {
 		esQuery.SetTerm("tags_level_two.keyword", secondTags)
@@ -363,7 +413,7 @@ func (receiver *EsAuthorBusiness) DyAuthorFollowerIncRank(date, tags, province, 
 	if orderBy == "" {
 		orderBy = "desc"
 	}
-	if !utils.InArrayString(sortStr, []string{"live_inc_follower_count", "inc_follower_count", "aweme_inc_follower_count","follower_count"}) {
+	if !utils.InArrayString(sortStr, []string{"live_inc_follower_count", "inc_follower_count", "aweme_inc_follower_count", "follower_count"}) {
 		comErr = global.NewError(4000)
 		return
 	}
