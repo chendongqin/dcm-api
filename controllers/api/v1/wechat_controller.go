b11cbedb7a53603b87868418619fdf172ed0a149
linchupeng <linchupeng@chandashi.com>
2021-08-28 13:24:45 +0800

    fix:修复 redis 登录 key

--- controllers/api/v1/wechat_controller.go
+++ controllers/api/v1/wechat_controller.go
@@ -50,7 +50,8 @@ func (receiver *WechatController) CheckScan() {
 		return
 	}
 	//从缓存中获取用户openId
-	cacheKey := "openid:qrscene_" + sessionId
+	cacheKey := "openid:" + sessionId
+
 	openId := global.Cache.Get(cacheKey)
 	if openId == "" {
 		receiver.FailReturn(global.NewError(4006))
