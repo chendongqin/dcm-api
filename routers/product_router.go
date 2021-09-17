a0a8487621e80c962aeb7b03a6970003670975af
linxiaobo <linxb@v-data.com>
2021-09-17 18:03:17 +0800

    feature:商品粉丝分析

--- routers/product_router.go
+++ routers/product_router.go
@@ -12,6 +12,7 @@ func init() {
 			beego.NSRouter("/search", &v1dy.ProductController{}, "get:Search"),
 			beego.NSRouter("/base/:product_id", &v1dy.ProductController{}, "get:ProductBase"),
 			beego.NSRouter("/analysis/:product_id/:start/:end", &v1dy.ProductController{}, "get:ProductBaseAnalysis"),
+			beego.NSRouter("/fans/analysis/:product_id", &v1dy.ProductController{}, "get:ProductFanAnalyse"),
 			beego.NSRouter("/live/chart/:product_id/:start/:end", &v1dy.ProductController{}, "get:ProductLiveChart"),
 			beego.NSRouter("/live/room/:product_id/:start/:end", &v1dy.ProductController{}, "get:ProductLiveRoomList"),
 			beego.NSRouter("/author/:product_id/:start/:end", &v1dy.ProductController{}, "get:ProductLiveAuthorAnalysis"),
