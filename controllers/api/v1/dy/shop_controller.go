4c47ea064bf913c29388b754bd3c613d80af4f4a
dong <dong@163.com>
2021-09-19 21:59:28 +0800

    fix:冲突处理

--- controllers/api/v1/dy/shop_controller.go
+++ controllers/api/v1/dy/shop_controller.go
@@ -5,9 +5,11 @@ import (
 	controllers "dongchamao/controllers/api"
 	"dongchamao/global"
 	"dongchamao/hbase"
+	"dongchamao/models/entity"
 	"dongchamao/models/repost/dy"
 	dy2 "dongchamao/models/repost/dy"
 	"sort"
+	"time"
 )
 
 type ShopController struct {
@@ -20,9 +22,36 @@ func (receiver *ShopController) Prepare() {
 	receiver.CheckDyUserGroupRight(business.DyJewelBaseMinShowNum, business.DyJewelBaseShowNum)
 }
 
+//小店基本数据
 func (receiver *ShopController) ShopBase() {
+	var returnRes entity.DyShopBaseBasic
+	var comErr global.CommonError
 	shopId := business.IdDecrypt(receiver.Ctx.Input.Param(":shop_id"))
-	receiver.SuccReturn(shopId)
+	if shopId == "" {
+		receiver.FailReturn(global.NewError(4000))
+		return
+	}
+	returnRes.BaseData, comErr = hbase.GetShop(shopId)
+	if comErr != nil {
+		receiver.FailReturn(comErr)
+		return
+	}
+	shopDetailData, comErr := hbase.GetShopDetailByDate(shopId, time.Now().Format("20060102"))
+	if comErr != nil { //今天取不到，取昨日数据
+		shopDetailData, _ = hbase.GetShopDetailByDate(shopId, time.Now().AddDate(0, 0, -1).Format("20060102"))
+	}
+
+	returnRes.DetailData.Sales = shopDetailData.Sales
+	returnRes.DetailData.Gmv = shopDetailData.Gmv
+	returnRes.DetailData.D30LiveCnt = shopDetailData.D30LiveCnt
+	returnRes.DetailData.D30AuthorCnt = shopDetailData.D30AuthorCnt
+	returnRes.DetailData.D30AwemeCnt = shopDetailData.D30AwemeCnt
+	returnRes.DetailData.D30Sales = shopDetailData.D30Sales
+	returnRes.DetailData.D30Gmv = shopDetailData.D30Gmv
+	returnRes.DetailData.D30Pct = shopDetailData.D30Pct
+
+	receiver.SuccReturn(returnRes)
+	return
 }
 
 /**小店数据基础分析 **/
@@ -126,5 +155,5 @@ func (receiver *ShopController) ShopBaseAnalysis() {
 		"price_chart":   priceSection,
 		"goods_cat_top": goodsCatTop,
 	})
-
+	return
 }
