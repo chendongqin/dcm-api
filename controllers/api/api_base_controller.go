0d143106c6e00b17e2aad1b9c8aab238b1cd03ec
dong <dong@163.com>
2021-09-23 14:07:09 +0800

    fix:返回结果加密

--- controllers/api/api_base_controller.go
+++ controllers/api/api_base_controller.go
@@ -234,8 +234,11 @@ func (this *ApiBaseController) CheckSign() {
 		if authBusiness.AuthSignWhiteUri(this.TrueUri) {
 			return
 		}
-		if global.JsonResEncrypt() {
-			this.JsonEncrypt = true
+		//todo 加密目前开放app
+		if utils.InArrayInt(this.AppId, []int{10003, 10004}) {
+			if global.JsonResEncrypt() {
+				this.JsonEncrypt = true
+			}
 		}
 		if global.IsDev() {
 			return
