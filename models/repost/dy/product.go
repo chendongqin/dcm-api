2bf2123e6184fca38a2513c315f8e06d774b4e4f
zhongleren <renzl@v-data.cn>
2021-09-23 15:53:33 +0800

    商品详情价格的问题，逻辑与商品库保持一致

--- models/repost/dy/product.go
+++ models/repost/dy/product.go
@@ -1,22 +1,24 @@
 package dy
 
 type SimpleDyProduct struct {
-	ProductID     string  `json:"product_id"`
-	Title         string  `json:"title"`
-	MarketPrice   float64 `json:"market_price"`
-	Price         float64 `json:"price"`
-	URL           string  `json:"url"`
-	Image         string  `json:"image"`
-	Status        int     `json:"status"`
-	ShopId        string  `json:"shop_id"`
-	ShopName      string  `json:"shop_name"`
-	Undercarriage int     `json:"undercarriage"`
-	CrawlTime     int64   `json:"crawl_time"`
-	PlatformLabel string  `json:"platform_label"`
-	Label         string  `json:"label"`
-	MinPrice      float64 `json:"min_price"`
-	CosRatio      float64 `json:"cos_ratio"`
-	CosRatioMoney float64 `json:"cos_ratio_money"`
+	ProductID           string  `json:"product_id"`
+	Title               string  `json:"title"`
+	MarketPrice         float64 `json:"market_price"`
+	Price               float64 `json:"price"`
+	URL                 string  `json:"url"`
+	Image               string  `json:"image"`
+	Status              int     `json:"status"`
+	ShopId              string  `json:"shop_id"`
+	ShopName            string  `json:"shop_name"`
+	Undercarriage       int     `json:"undercarriage"`
+	CrawlTime           int64   `json:"crawl_time"`
+	PlatformLabel       string  `json:"platform_label"`
+	Label               string  `json:"label"`
+	MinPrice            float64 `json:"min_price"`
+	CosRatio            float64 `json:"cos_ratio"`
+	CosRatioMoney       float64 `json:"cos_ratio_money"`
+	TbCouponPrice       float64 `json:"tb_coupon_price"`
+	TbCouponRemainCount int64   `json:"tb_coupon_remain_count"`
 }
 
 type ProductOrderDaily struct {
