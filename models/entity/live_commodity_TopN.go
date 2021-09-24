fa41f5f7a9b0e06191635906fef084b92f4cd9e4
sujx <sujx@v-data.cn>
2021-09-24 20:11:03 +0800

    modify:直播商品榜字段

--- models/entity/live_commodity_TopN.go
+++ models/entity/live_commodity_TopN.go
@@ -1,5 +1,15 @@
 package entity
 
+var LiveCommodityTopNMap = HbaseEntity{
+	"info":    {Long, "update_time"},
+	"message": {AJson, "ranks"},
+}
+
+type LiveCommodityTopN struct {
+	UpdateTime int64         `json:"update_time"`
+	Ranks      []LiveProduct `json:"ranks"`
+}
+
 type DyLiveProductSaleTopRank struct {
 	ProductId     string  `json:"product_id"`
 	DateTime      string  `json:"date_time"`
@@ -15,4 +25,17 @@ type DyLiveProductSaleTopRank struct {
 	PlatformLabel string  `json:"platform_label"`
 	Undercarriage int     `json:"undercarriage"`
 	LiveCount     int64   `json:"live_count"`
+}
+
+type LiveProduct struct {
+	Image         string  `json:"image"`
+	Saleroom      float64 `json:"saleroom"`
+	RoomNum       int64   `json:"room_num"`
+	CosRatio      float64 `json:"cos_ratio"`
+	Price         float64 `json:"price"`
+	ProductId     string  `json:"product_id"`
+	CosFee        float64 `json:"cos_fee"`
+	Title         string  `json:"title"`
+	Sales         int64   `json:"sales"`
+	PlatformLabel string  `json:"platform_label"`
 }
