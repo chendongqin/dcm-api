c627122430f8021a26832377a0812884c9e445f7
dong <dong@163.com>
2021-09-25 16:12:04 +0800

    fix:达人概览数据处理

--- business/es/es_live_business.go
+++ business/es/es_live_business.go
@@ -102,16 +102,79 @@ func (receiver *EsLiveBusiness) CountRoomProductByAuthorId(authorId string, star
 		"gte": startTime.Unix(),
 		"lt":  endTime.AddDate(0, 0, 1).Unix(),
 	})
-	_ = esMultiQuery.
+	countResult := esMultiQuery.
 		SetConnection(connection).
+		SetCache(180).
 		SetTable(esTable).
-		SetCollapse("product_id.keyword").
-		SetFields("product_id").
-		AddMust(esQuery.Condition).
-		SetMultiQuery().
-		Query()
-	total := esMultiQuery.Count
-	return int64(total)
+		RawQuery(map[string]interface{}{
+			"query": map[string]interface{}{
+				"bool": map[string]interface{}{
+					"must": esQuery.Condition,
+				},
+			},
+			"size": 0,
+			"aggs": map[string]interface{}{
+				"product": map[string]interface{}{
+					"terms": map[string]interface{}{
+						"field": "product_id.keyword",
+						"size":  1,
+					},
+				},
+			},
+		})
+	var total int64 = 0
+	if h, ok := countResult["hits"]; ok {
+		if t, ok2 := h.(map[string]interface{})["total"]; ok2 {
+			total = utils.ToInt64(t.(float64))
+		}
+	}
+	return total
+}
+
+//直播间场次统计（天）
+func (receiver *EsLiveBusiness) CountRoomByDayByAuthorId(authorId string, hasProduct int, startTime, endTime time.Time) int64 {
+	esTable, connection, err := GetESTableByTime(es.DyLiveInfoBaseTable, startTime, endTime)
+	if err != nil {
+		return 0
+	}
+	esQuery, esMultiQuery := elasticsearch.NewElasticQueryGroup()
+	esQuery.SetTerm("author_id", authorId)
+	esQuery.SetRange("create_time", map[string]interface{}{
+		"gte": startTime.Unix(),
+		"lt":  endTime.AddDate(0, 0, 1).Unix(),
+	})
+	if hasProduct == 1 {
+		esQuery.SetRange("num_product", map[string]interface{}{
+			"gt": 0,
+		})
+	}
+	countResult := esMultiQuery.
+		SetConnection(connection).
+		SetCache(300).
+		SetTable(esTable).
+		RawQuery(map[string]interface{}{
+			"query": map[string]interface{}{
+				"bool": map[string]interface{}{
+					"must": esQuery.Condition,
+				},
+			},
+			"size": 0,
+			"aggs": map[string]interface{}{
+				"live": map[string]interface{}{
+					"terms": map[string]interface{}{
+						"field": "dt.keyword",
+						"size":  1,
+					},
+				},
+			},
+		})
+	var total int64 = 0
+	if h, ok := countResult["hits"]; ok {
+		if t, ok2 := h.(map[string]interface{})["total"]; ok2 {
+			total = utils.ToInt64(t.(float64))
+		}
+	}
+	return total
 }
 
 //直播间筛选
