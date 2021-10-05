88dae88adb6a1a6e6814a3a27ead154adf59be37
linxiaobo <linxb@v-data.com>
2021-10-05 19:22:52 +0800

    feature:去除归因代码

--- controllers/api/v1/common_controller.go
+++ controllers/api/v1/common_controller.go
@@ -15,7 +15,6 @@ import (
 	"dongchamao/services/ali_sms"
 	"dongchamao/services/ali_tools"
 	"dongchamao/services/dyimg"
-	"dongchamao/services/tencent_ad"
 	"encoding/json"
 	"fmt"
 	jsoniter "github.com/json-iterator/go"
@@ -137,7 +136,6 @@ func (receiver *CommonController) CheckSmsCode() {
 }
 
 func (receiver *CommonController) Test() {
-	tencent_ad.AddUserActionSets("0024")
 	receiver.SuccReturn(nil)
 	return
 }
