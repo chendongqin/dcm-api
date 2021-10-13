e4d73a87d7b84a4860e755a0b8c101d03589dbe8
dong <dong@163.com>
2021-10-13 20:46:03 +0800

    fix:商品统计

--- controllers/api/v1/dy/product_controller.go
+++ controllers/api/v1/dy/product_controller.go
@@ -161,9 +161,16 @@ func (receiver *ProductController) ProductBaseAnalysis() {
 	info, _ := hbase.GetProductDailyRangDate(productId, startTime, endTime)
 	monthData, _ := hbase.GetPromotionMonth(productId)
 	dailyMapData := map[string]entity.DyLiveProductDaily{}
-	for _, v := range monthData.DailyList {
+	dailyList := monthData.DailyList
+	sort.Slice(dailyList, func(i, j int) bool {
+		return dailyList[i].StatisticsTime > dailyList[j].StatisticsTime
+	})
+	for _, v := range dailyList {
 		t, _ := time.ParseInLocation("2006/01/02", v.StatisticsTime, time.Local)
-		if t.Before(startTime) || t.After(endTime) {
+		if t.Unix() < startTime.Unix() {
+			break
+		}
+		if t.Unix() > endTime.Unix() {
 			continue
 		}
 		dailyMapData[t.Format("20060102")] = v
@@ -365,10 +372,10 @@ func (receiver *ProductController) ProductBase() {
 	var pvCount30 int64 = 0
 	for _, v := range dailyList {
 		t, _ := time.ParseInLocation("2006/01/02", v.StatisticsTime, time.Local)
-		if t.Before(startTime) {
+		if t.Unix() < startTime.Unix() {
 			break
 		}
-		if t.After(yesterdayTime) {
+		if t.Unix() > yesterdayTime.Unix() {
 			continue
 		}
 		orderCount30 += v.ProductOrderAccount
