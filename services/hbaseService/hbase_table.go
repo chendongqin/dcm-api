0a07475dae8a1c31eaa04a8deda1ad70f036b655
linxiaobo <linxb@v-data.com>
2021-10-15 14:49:48 +0800

    fix:商品销量榜

--- services/hbaseService/hbase_table.go
+++ services/hbaseService/hbase_table.go
@@ -59,6 +59,7 @@ const (
 	HbaseDyShopDetail          = "dy_shop_detail"           //小店详情
 	HbaseDyShopProductAnalysis = "dy_shop_product_analysis" //小店商品分析
 
+	HbaseProductSellRank        = "dy_commodity_TopN"           //抖音销量榜
 	HbaseShortVideoProductRank  = "shortvideo_commodity_topN"   //视频商品榜
 	HbaseLiveProductRank        = "live_commodity_TopN"         //直播商品榜
 	HbaseDyAuthorDaySalesRank   = "dy_author_day_sales"         //达人带货日榜
